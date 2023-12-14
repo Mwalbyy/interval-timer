09e21b30fc457bfeca9dfeb9e4c7caa99fb32d5a
Marcus Walby <marcuswalby@marcuss-mbp.myfiosgateway.com>
2023-12-14 17:21:45 -0500

    set timer

--- interval-timer/src/App.js
+++ interval-timer/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Timer from './components/Timer';
 
 function App() {
--- interval-timer/src/components/Timer/index.js
+++ interval-timer/src/components/Timer/index.js
@@ -4,6 +4,8 @@ const Timer = () => {
     const [seconds, setSeconds] = useState(0);
     const [timeLimit, setTimeLimit] = useState(1);
     const [isRunning, setIsRunning] = useState(false);
+    const [repeatCount, setRepeatCount] = useState(1);
+    const [timeBetweenRepeats, setTimeBetweenRepeats] = useState(0);
 
     useEffect(() => {
         let interval = null;
@@ -13,6 +15,13 @@ const Timer = () => {
                 setSeconds((prevSeconds) => {
                     if (prevSeconds >= timeLimit) {
                         clearInterval(interval);
+                        if (repeatCount > 1) {
+                            setTimeout(() => {
+                                setSeconds(0);
+                                setIsRunning(true);
+                                setRepeatCount((prevCount) => prevCount - 1);
+                            }, timeBetweenRepeats * 1000);
+                        }
                         return prevSeconds;
                     }
                     return prevSeconds + 1;
@@ -22,11 +31,21 @@ const Timer = () => {
             clearInterval(interval);
         }
 
-        return () => clearInterval(interval);
-    }, [isRunning, timeLimit]);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isRunning, timeLimit, repeatCount, timeBetweenRepeats]);
 
     const handleTimeLimitChange = (event) => {
         setTimeLimit(parseInt(event.target.value));
+    };
+
+    const handleRepeatCountChange = (event) => {
+        setRepeatCount(parseInt(event.target.value));
+    };
+
+    const handleTimeBetweenRepeatsChange = (event) => {
+        setTimeBetweenRepeats(parseInt(event.target.value));
     };
 
     const handleStart = () => {
@@ -40,11 +59,14 @@ const Timer = () => {
     const handleReset = () => {
         setIsRunning(false);
         setSeconds(0);
+        setRepeatCount(1);
+        setTimeBetweenRepeats(0);
     };
 
     return (
         <div>
-            <h1>Timer: {seconds} seconds</h1>
+            <h1 className={isRunning ? 'timerRunning' : 'timer'}>Timer: {seconds} seconds</h1>
+            
             <button onClick={handleStart}>Start</button>
             <button onClick={handleStop}>Stop</button>
             <button onClick={handleReset}>Reset</button>
@@ -60,9 +82,32 @@ const Timer = () => {
                     ))}
                 </select>
             </label>
+            <br />
+            <br />
+            <label>
+                Select Repeat Count:
+                <select value={repeatCount} onChange={handleRepeatCountChange}>
+                    {Array.from({ length: 10 }, (_, i) => i + 1).map((value) => (
+                        <option key={value} value={value}>
+                            {value}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            <br />
+            <br />
+            <label>
+                Select Time Between Repeats (in seconds):
+                <select value={timeBetweenRepeats} onChange={handleTimeBetweenRepeatsChange}>
+                    {Array.from({ length: 10 }, (_, i) => i).map((value) => (
+                        <option key={value} value={value}>
+                            {value}
+                        </option>
+                    ))}
+                </select>
+            </label>
         </div>
     );
 };
 
 export default Timer;
-
